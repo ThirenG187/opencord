fa1a9fc091d846ed9c1d26db97cdac44a75d952c
Thiren Govender <theron619@gmail.com>
2023-08-26 14:04:33 +0200

    * Added Theme provider * Initiated prisma schema

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,12 @@
 import { UserButton } from '@clerk/nextjs'
 
+import { ModeToggle } from '@/components/mode-toggle'
+
 const HomePage = () => {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   )
 }
